cebf468553086afcd2c367e621f296dd990482bd
MUHAMMED FAZIL PM <fazilfezz@gmail.com>
2024-05-01 09:53:24 +0530

    updated nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
         <div className={`w-full md:flex md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:mt-0 md:flex-row md:space-x-8 rtl:space-x-reverse">
             <li>
-              <a onClick={() => {navigate('/')}} className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-black md:dark:hover:text-blue-500" aria-current="page">HOME</a>
+              <a onClick={() => {navigate('/')}} className="block py-2 px-3 text-black bg-transparent rounded md:bg-transparent md:text-black md:p-0  hover:text-blue-500 dark:text-white md:dark:text-black md:dark:hover:text-blue-500" aria-current="page">HOME</a>
             </li>
             <li>
               <a onClick={() => {navigate('/about')}} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-900 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">ABOUT US</a>
