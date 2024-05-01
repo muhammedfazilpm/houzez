4191d85738e668b0cc84ca5c0433ce0dba2cf755
MUHAMMED FAZIL PM <fazilfezz@gmail.com>
2024-05-01 11:00:11 +0530

    updated navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,14 @@ function Home() {
       <div className="firstimage">
         <Navbarwhite />
         <div className="center-div  text-white ">
+        <div>
           <h3>How Much Is My House Worth?​</h3>
           <p>
             Houzez was built to get more real estate leads, increase your
             conversions rate, schedule more viewings for your properties and
             ultimately earn more commissions
           </p>
+          </div>
           <button className="bg-blue-600 pr-6 pl-6 p-2 mt-8">
             Get Started
           </button>
